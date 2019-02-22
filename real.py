510f8f9f6b01c7d55321363739cedba2342ed9d2
Rohit Kumar <44257572+Rajgeel@users.noreply.github.com>
2019-02-23 00:16:38 +0530

    Change in print order and add come other print

--- real.py
+++ real.py
@@ -15,24 +15,22 @@ def main():
 
 @app.route('/project', methods = ['POST'])
 def project():
-	print("1")
 	if request.method == 'POST':
-		print("2")
-		# print(request)
 		db = client['school']
 		col = db['students']
-		print(request.form["name"])
-		print(request.form["roll_no"])
-		print(request.form["sub_code"])
 		es = col.find_one({"Roll_No" : request.form["roll_no"], "Subject_Code" : request.form["sub_code"]})
-		print(es)
 		if es is None:
-			dist = ({"Name" : request.form["name"], "Roll_No" : request.form["roll_no"], "Semester" : request.form["sem"], "Subject_Code" : request.form["sub_code"], "Lecture_Quality" : request.form["lec"], "Assignment_Quality" : request.form["assign"]})
+			dist = ({"Name" : request.form["name"], "Roll_No" : request.form["roll_no"], "Semester" : request.form["sem"], "Subject_Code" : request.form["sub_code"], "Lecture_Quality" : request.form["lec"], "Assignment_Quality" : request.form["assign"], "Any Comment" : request.form["otherText"]})
 			col.insert_one(dist)
-			print("3")
+			print(request.form["name"])
+			print(request.form["roll_no"])
+			print(request.form["sem"])
+			print(request.form["sub_code"])
+			print(request.form["lec"])
+			print(request.form["assign"])
+			print(request.form["otherText"])
 			return render_template("submit.html")
 		else:
-			print("4")
 			return render_template("exist.html")
 	
 if __name__ == '__main__':
